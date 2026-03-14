9ecde2679b1ce7ccd91c18d2145cce84edc89f91
pavanhs1ga22ad035-sudo <pavan.h.s1ga22ad035@gmail.com>
2026-03-14 19:29:41 +0530

    Add modular my-learning class list with accordion and video player

---
+++ frontend/app/my-learning/class/list/page.tsx
@@ -0,0 +1,5 @@
+import MyLearningClassListPage from '../../../../components/learning/MyLearningClassListPage';
+
+export default function MyLearningClassListRoute() {
+  return <MyLearningClassListPage />;
+}
